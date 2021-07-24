25b660964fab90d33efca9e18658896075258faa
Hirofumi Seo <seo@sciement.com>
2021-07-24 22:38:55 +0900

    Revert to pResource. With interop. Still so many memory leaks.

--- Samples/simpleD3D11/simpleD3D11.cpp
+++ Samples/simpleD3D11/simpleD3D11.cpp
@@ -70,9 +70,7 @@ ID3D11VertexShader     *g_pVertexShader;
 ID3D11PixelShader      *g_pPixelShader;
 ID3D11InputLayout      *g_pLayout;
 ID3D11Buffer           *g_VertexBuffer;
-IDXGIResource1         *g_pVertexBufferResource;
 IDXGIKeyedMutex        *g_pKeyedMutex11;
-IDXGIResource1         *g_pKeyedMutex11Resource;
 
 
 Vertex *d_VertexBufPtr = NULL;
@@ -502,23 +500,23 @@ HRESULT InitD3D(HWND hWnd)
     g_pd3dDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_POINTLIST);
     AssertOrQuit(SUCCEEDED(hr));
 
-    //IDXGIResource1* pResource;
+    IDXGIResource1* pResource;
     HANDLE sharedHandle;
-    g_VertexBuffer->QueryInterface(__uuidof(IDXGIResource1), (void**)&g_pVertexBufferResource);
-    hr = g_pVertexBufferResource->GetSharedHandle(&sharedHandle);
+    g_VertexBuffer->QueryInterface(__uuidof(IDXGIResource1), (void**)&pResource);
+    hr = pResource->GetSharedHandle(&sharedHandle);
     if (!SUCCEEDED(hr))
     {
         std::cout << "Failed GetSharedHandle hr= " << hr << std::endl;
     }
     // Import the D3D11 Vertex Buffer into CUDA
-//    d_VertexBufPtr = cudaImportVertexBuffer(sharedHandle, extMemory, g_WindowWidth, g_WindowHeight);
-    g_pVertexBufferResource->Release();
+    d_VertexBufPtr = cudaImportVertexBuffer(sharedHandle, extMemory, g_WindowWidth, g_WindowHeight);
+    pResource->Release();
 
-    g_pKeyedMutex11->QueryInterface(__uuidof(IDXGIResource1), (void**)&g_pKeyedMutex11Resource);
-    g_pKeyedMutex11Resource->GetSharedHandle(&sharedHandle);
+    g_pKeyedMutex11->QueryInterface(__uuidof(IDXGIResource1), (void**)&pResource);
+    pResource->GetSharedHandle(&sharedHandle);
     // Import the D3D11 Keyed Mutex into CUDA
-//    cudaImportKeyedMutex(sharedHandle, extSemaphore);
-    g_pKeyedMutex11Resource->Release();
+    cudaImportKeyedMutex(sharedHandle, extSemaphore);
+    pResource->Release();
 
     D3D11_RASTERIZER_DESC rasterizerState;
     rasterizerState.FillMode = D3D11_FILL_SOLID;
@@ -603,16 +601,6 @@ void Cleanup()
         g_VertexBuffer->Release();
     }
 
-    if (g_pVertexBufferResource)
-    {
-        g_pVertexBufferResource->Release();
-    }
-
-    if (g_pKeyedMutex11Resource)
-    {
-        g_pKeyedMutex11Resource->Release();
-    }
-
     if (g_pKeyedMutex11)
     {
         g_pKeyedMutex11->Release();
@@ -650,7 +638,7 @@ void Render()
     static uint64_t key = 0;
 
     // Launch cuda kernel to generate sinewave in vertex buffer
-//    RunSineWaveKernel(extSemaphore, key, INFINITE, g_WindowWidth, g_WindowWidth, d_VertexBufPtr, cuda_stream);
+    RunSineWaveKernel(extSemaphore, key, INFINITE, g_WindowWidth, g_WindowWidth, d_VertexBufPtr, cuda_stream);
 
     // Draw the scene using them
     DrawScene(key);
